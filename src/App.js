a41c975e3f49f0de7a3a61610794d61714bb096e
jeffreyadamo <jeffreyadamo@gmail.com>
2021-03-04 22:11:46 -0800

    scrapped default landing page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
-import logo from './logo.svg';
+import happyMushroom from './happyMushroom.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={happyMushroom} className="App-logo" alt="logo" />
+        <br></br>
+        <br></br>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         Mushroom Facts is under construction
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
